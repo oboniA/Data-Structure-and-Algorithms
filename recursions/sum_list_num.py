84a9839a41c2a75c8463685c67d579be30fdeedc
Oboni ANower <oboni_anower@yahoo.com>
2022-07-25 22:30:24 +0100

    Sum of a list of numbers

---
+++ recursions/sum_list_num.py
@@ -0,0 +1,14 @@
+"""
+Write a Python program to calculate the sum of a list of numbers.
+"""
+
+
+def sums(num_list):
+    if len(num_list) == 1:  # if the list has only 1 element; length is 1
+        return num_list[0]  # return the base case (first/only element)
+    else:
+        # returns the first case + the sum of the remaining elements
+        return num_list[0] + sums(num_list[1:])
+
+
+print(sums([2, 4, 5, 6, 7]))
