8a5f0dc8efb61d038b64decf1039a48d8f0761d7
Oboni ANower <oboni_anower@yahoo.com>
2022-07-26 16:56:17 +0100

    creation of lists

---
+++ LIST/constructors.py
@@ -0,0 +1,24 @@
+x1 = []      # created an empty list
+print(x1)
+
+x2 = [2, 4, 6, 7, 8]
+print(x2)
+
+x3 = ['qq', 'ss', 'kk']
+print(x3)
+
+x3 = ['gg', 5, 'g5', 7.7]
+print(x3)
+
+
+
+x = list()   # created an empty list
+print(x)
+
+tuple1 = (23, 45, 'ww')      # created a tuple
+print(tuple1)
+
+y = list(tuple1)      # add the tuple to the empty list
+print(y)
+
+
