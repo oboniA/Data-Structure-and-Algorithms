348a6c9713b2925db013016cedc61e2dc6fc88bf
Oboni ANower <oboni_anower@yahoo.com>
2022-07-26 15:10:33 +0100

    multiplying indx

---
+++ list_string_tuple/multiply.py
@@ -0,0 +1,19 @@
+# string
+x = 'cat' * 5
+print(x)
+
+
+# list
+y = [4, 5] * 3
+print(y)
+
+y2 = ['aa', 'bb'] * 3
+print(y2)
+
+
+# tuple
+z = (4, 5) * 3
+print(z)
+
+z2 = ('aa', 'bb') * 3
+print(z2)
