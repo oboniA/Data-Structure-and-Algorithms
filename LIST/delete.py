b42f956c761e2aaf330d6df1cbb2c79e10705e93
Oboni ANower <oboni_anower@yahoo.com>
2022-07-26 16:55:53 +0100

    removing stuff from list

---
+++ LIST/delete.py
@@ -0,0 +1,19 @@
+x = [2, 4, 6, 7, 8]  # list created
+del(x[2])            # selected the index2 o delete
+print(x)             # print list after deleting index2
+
+x.remove(6)          # removes the 6 from list
+print(x)
+
+x.pop()              # removes the last element
+print(x)
+
+
+
+
+del(x)               # deletes the entire list
+print(x)
+
+
+x.clear()            # clears the list
+print(x)
