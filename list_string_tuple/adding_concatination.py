405b0f7ae49dad0ca6a1b8d545c2b46ce2984e01
Oboni ANower <oboni_anower@yahoo.com>
2022-07-26 15:11:56 +0100

    concatination

---
+++ list_string_tuple/adding_concatination.py
@@ -0,0 +1,13 @@
+x = 'computer' + ' ' + 'Laptop'
+print(x)
+print(x[12])
+
+# list
+y = ['keyboard', 'mouse', 'speaker', 'monitor'] + ['keypad', 'USB']
+print(y)
+print(y[5])
+
+# tuple
+z = ('keyboard', 'mouse', 'speaker', 'monitor') + ('keypad', 'USB')
+print(z)
+print(z[3])
