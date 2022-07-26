4536715980af938954bbedebcd6f7c9f2b4bf51e
Oboni ANower <oboni_anower@yahoo.com>
2022-07-26 15:12:41 +0100

    check if an element is present or not ans in boolean

---
+++ list_string_tuple/check_if_present.py
@@ -0,0 +1,20 @@
+"""
+Booleans
+"""
+
+# string
+x = 'computer.. '
+print('t' in x)
+print('s' in x)
+
+# list
+y = ['keyboard', 'mouse', 'speaker', 'monitor']
+print('mouse' in y)
+print('keyboard' not in y)
+print('rat' in y)
+
+# tuple
+z = ('keyboard', 'mouse', 'speaker', 'monitor')
+print('mouse' in z)
+print('keyboard' not in z)
+print('rat' in z)
