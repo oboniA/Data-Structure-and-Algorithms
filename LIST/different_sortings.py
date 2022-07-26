f104e6aef564311677778d7f413b59b1e3ff4984
Oboni ANower <oboni_anower@yahoo.com>
2022-07-26 16:57:16 +0100

    sorting the data in list

---
+++ LIST/different_sortings.py
@@ -0,0 +1,22 @@
+x = [2, 4, 6, 7, 8]
+x.reverse()         # prints in reverse order
+print(x)
+
+
+"""
+sorted(x) returns new sorted list without changing content
+x.sort() puts the items of x in sorted order (sorts in place)
+"""
+y = [2, 4, 3, 9, 8]
+# print(sorted(y))
+y.sort()
+print(y)
+
+
+
+"""
+Reverse sort
+"""
+z = [2, 4, 3, 9, 8]
+z.sort(reverse=True)
+print(x)
