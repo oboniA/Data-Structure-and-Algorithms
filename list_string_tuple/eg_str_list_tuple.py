48a55d99078dc2adf99d77707a799cf0aa155efc
Oboni ANower <oboni_anower@yahoo.com>
2022-07-26 15:11:14 +0100

    examples of list string and tuple

---
+++ list_string_tuple/eg_str_list_tuple.py
@@ -0,0 +1,11 @@
+# string
+x = 'computer'
+print(x[4])
+
+# list
+y = ['keyboard', 'mouse', 'speaker', 'monitor']
+print(y[3])
+
+# tuple
+z = ('keyboard', 'mouse', 'speaker', 'monitor')
+print(y[3])
