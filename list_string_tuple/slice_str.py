335d92fe60fa0b9fd6e4a0706c811dfc34745264
Oboni ANower <oboni_anower@yahoo.com>
2022-07-26 15:09:22 +0100

    slice

---
+++ list_string_tuple/slice_str.py
@@ -0,0 +1,21 @@
+"""
+ [start : end+1 : step]
+"""
+
+x = 'Computer Science'
+
+print(x[4])        # prints index4
+
+print(x[3:7])      # from index2 to index6
+
+print(x[1:10:2])   # from index1 to index10, with a 2-step
+
+print(x[3:])       # everything after index3
+
+print(x[:10])      # everything before index10
+
+print(x[-1])       # last index
+
+print(x[-3:])      # last 3 indexes
+
+print(x[:-2])      # everything before the last 2 indexes
