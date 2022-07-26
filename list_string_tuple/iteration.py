fcb53cafe0a6fa141c71c2064fe83e5d7691cbb6
Oboni ANower <oboni_anower@yahoo.com>
2022-07-26 15:19:22 +0100

    itiration

---
+++ list_string_tuple/iteration.py
@@ -0,0 +1,10 @@
+
+x = [3, 4, 5, 8]
+for item in x:      # an item present in list x
+    print(item)     # print all the items
+
+
+# gives the items and the index of them
+y = [3, 4, 5, 7]
+for item, index in enumerate(y):
+    print(item, index)
