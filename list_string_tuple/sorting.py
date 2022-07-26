0be88ab35aaf993670a3560dbb3e3b5acd08c2d2
Oboni ANower <oboni_anower@yahoo.com>
2022-07-26 15:32:43 +0100

    sorting the numbers at given posotion/s

---
+++ list_string_tuple/sorting.py
@@ -0,0 +1,20 @@
+"""
+ * takes a string, returns it as list of elements in ascending order
+ * returns a list in ascending order
+ * returns a tuple in ascending order
+"""
+
+x = 'computer'
+print(sorted(x))
+
+# list
+y = ['keyboard', 'mouse', 'speaker', 'monitor']
+print(sorted(y))
+
+# tuple
+z = ('keyboard', 'mouse', 'speaker', 'monitor')
+print(sorted(z))
+
+# can be sorted at specific position, in this case, the 3rd letter
+z2 = ('keyboard', 'mouse', 'speaker', 'monitor')
+print(sorted(z, key=lambda k: k[2]))
