86c67d73e2732714ebf224a3cbe7b6fcbe8cd1ac
Oboni ANower <oboni_anower@yahoo.com>
2022-07-27 17:45:55 +0100

    minimize the number of times we access elements from the list using BINARY SEARCH

--- LIST/Exercises/decreasing_order.py
+++ LIST/Exercises/decreasing_order.py
@@ -1,26 +1,36 @@
 """
-write a program to find the position of a given number 
-in a list of numbers arranged in decreasing order.
-We also need to minimize the number of times we access elements from the list.
+* write a program to find the position of a given number
+  in a list of numbers arranged in decreasing order.
+* We also need to minimize the number of times we access elements from the list.
 
 list : cards
 int : search  [find a certain value from a list called cards
+
+ BINARY SEARCH
+ - run-time is long for higher values
+ - Big O is O(log N)
 """
 
 
-def find_card(cards, search_card):
-    card_position = 0
+def find_card(cards, search_card_value):
+    low = 0
+    high = len(cards) - 1
+    """ can be written as 
+        low, high = 0, len(cards) - 1
+    """
 
-    print('Deck of cards: ', cards)
-    print(f"find the position of card {search_card} from the deck")
+    while low < high:
+        mid = (low+high) // 2
+        mid_card_value = cards[mid]
 
-    # use <, not <=; else it will return error, instead of while-return
-    while card_position < len(cards):            # searching  is on, not reached the end
-        if cards[card_position] == search_card:  # if the current index holds the card we are searching for
-            return card_position                 # print that index where the card is present
-        else:
-            card_position += 1                   # keeps searching the card, from one address to the next address
-    return f"Card {search_card} Does not exist in the deck"                     # if the search_card value doesn't exist
+        if mid_card_value == search_card_value:
+            return mid
+        elif mid_card_value < search_card_value:
+            high = mid - 1
+        elif mid_card_value > search_card_value:
+            high = mid + 1
+
+    return f"Card {search_card_value} Does not exist in the deck"    # if the search_card value doesn't exist
 
 
-print(find_card([19, 17, 15, 11, 8, 4, 2], 20))
+print(find_card([9, 7, 5, 2, 1], 5))
