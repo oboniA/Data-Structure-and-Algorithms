ad42a5f96fc40937f7d1cd85bca73e2092671a1a
Oboni ANower <oboni_anower@yahoo.com>
2022-08-10 17:44:15 +0100

    increasing order

---
+++ MIT-6.006/insertion_sort.py
@@ -0,0 +1,9 @@
+def insert_sort(list_a):
+    for i in range(1, len(list_a)):                              # for every value(i) within that length of given list
+        while list_a[i] < list_a[i-1] and i>0 :                  # when current value is smaller than the previous value (and value is greater than 0)
+            list_a[i], list_a[i-1] = list_a[i-1], list_a[i]      # swap the value, LHS with RHS
+            i -= 1                                               # continues the comparison and swap with all the elements
+
+    return list_a
+
+print(insert_sort([7, 5, 1, 3, 9, 10, 2]))
