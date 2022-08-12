d7244199dc59e4e155e1ffcad8c28e7d42e03252
Oboni ANower <oboni_anower@yahoo.com>
2022-08-12 17:04:11 +0100

    directory name changed

---
+++ list_string_tuple/constructing list_tuple.py
@@ -0,0 +1,22 @@
+x = list()  # created an empty list
+print(x)
+
+x1 = []     # created an empty list
+print(x1)
+
+x2 = [2, 4, 6, 7, 8]
+print(x2)
+
+x3 = ['qq', 'ss', 'kk']
+print(x3)
+
+x3 = ['gg', 5, 'g5', 7.7]
+print(x3)
+
+
+
+tuple1 = (23, 45, 'ww')      # created a tuple
+print(tuple1)
+
+y = list(tuple1)             # add the tuple to the empty list
+print(y)
