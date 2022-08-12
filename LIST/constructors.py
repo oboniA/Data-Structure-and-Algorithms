4c4f84a3eeb9a9de2c24bd301723c50852b19d11
Oboni ANower <oboni_anower@yahoo.com>
2022-08-12 17:05:42 +0100

    create lists in different ways

--- LIST/constructors.py
+++ LIST/constructors.py
@@ -1,4 +1,7 @@
-x1 = []      # created an empty list
+x = list()  # created an empty list
+print(x)
+
+x1 = []     # created an empty list
 print(x1)
 
 x2 = [2, 4, 6, 7, 8]
@@ -7,18 +10,11 @@ print(x2)
 x3 = ['qq', 'ss', 'kk']
 print(x3)
 
-x3 = ['gg', 5, 'g5', 7.7]
-print(x3)
+x4 = ['gg', 5, 'g5', 7.7]
+print(x4)
 
 
 
-x = list()   # created an empty list
-print(x)
 
-tuple1 = (23, 45, 'ww')      # created a tuple
-print(tuple1)
-
-y = list(tuple1)      # add the tuple to the empty list
-print(y)
 
 
