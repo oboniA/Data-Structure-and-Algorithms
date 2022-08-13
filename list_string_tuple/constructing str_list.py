a7cdb8b62b6b448ed51e20abd1172399ddfd7354
Oboni ANower <oboni_anower@yahoo.com>
2022-08-13 12:32:27 +0100

    convert string to a list

---
+++ list_string_tuple/constructing str_list.py
@@ -0,0 +1,11 @@
+# user input method
+str = input("enter a string to convert to list: ")
+str_to_list = str.split()
+
+print(str_to_list)
+
+# given method
+str2 = 'My name is Oboni'
+str_to_list2 = str2.split()
+
+print(str_to_list2)
