16e7a2c78747e99cd8508506d557cb292b8f9730
Oboni ANower <oboni_anower@yahoo.com>
2022-08-13 12:32:58 +0100

    multi dim list

--- LIST/multi-dimentional.py
+++ LIST/multi-dimentional.py
@@ -1,3 +1,4 @@
-coords = [[1,2,3,4], [5,6,7,8]]
+coords = [[1,2,3,4], [5,6,7,8]]  # 0th list: 0 1 2 3 index, 1st list: 0 1 2 3 index
 print(coords)
-print(coords[1][2])   # give[2, 4, 3, 9, 8]s from 1st Row and 2nd column
+print(coords[1][2])   # give the value from 1st Row and 2nd column
+                      # OR, gives, the index2 from 1st list
