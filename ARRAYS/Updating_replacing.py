f5c214dd6f83cf70e14a7e85c83ee117aec8c7f6
Oboni ANower <oboni_anower@yahoo.com>
2022-08-13 14:35:43 +0100

    updating index value

---
+++ ARRAYS/Updating_replacing.py
@@ -0,0 +1,14 @@
+import array as arr
+
+a = arr.array('i', [1, 2, 3, 1, 2, 5])
+print("Array before updation : ", end="")
+for i in range(0, len(a)):
+    print(a[i], end=" ")
+print()
+
+a[2] = 6        # replacing the index2 value with 6
+print("Array after updation : ", end ="")
+for i in range (0, len(a)):
+    print(a[i], end =" ")
+print()
+
