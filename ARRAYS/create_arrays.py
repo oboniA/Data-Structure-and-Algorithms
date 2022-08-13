21222bc19063c4272988565f98c15a1e91b0b682
Oboni ANower <oboni_anower@yahoo.com>
2022-08-13 13:23:14 +0100

    print in array form

--- ARRAYS/create_arrays.py
+++ ARRAYS/create_arrays.py
@@ -1,9 +1,16 @@
-""" importing array data type"""
+""" importing array data type
+    array(data_type, value_list)
+"""
 
 import array as arr1
 
 # a = function.module('',[])
 a = arr1.array('i',[1, 3, 67, 89, 10, 2])  # elements stored in a;
-                                           # 'i' = integer
+                                           # 'i' = integer; 'd' = float
 print(a)
 print(a[3])
+
+# now print the array in array/list form
+for i in range(0, 6):
+    print(a[i], end=" ")
+print()
