153914e564febd2f96e3f4cc72566ec7e77fe026
Oboni ANower <oboni_anower@yahoo.com>
2022-08-14 16:34:40 +0100

    Correction

--- LIST/Exercises/removes dupliocate.py
+++ LIST/Exercises/removes dupliocate.py
@@ -1,21 +1,21 @@
 """ removes duplicate elements from a given list
- return new list without the duplication """
+ return new list without the duplication
 
-nums = [1, 3, 67, 89, 1, 3, 45, 66]
-no_duplicate = []  # every number stored here will occur once
-
-"""
 every number will be checked, and see if it exists in the no_duplicate list,
-if doesn/t exist, will be added to the list,
+if doesn't exist, will be added to the list,
 if exists already, will be discarded
 will print the list with every number occurring only once 
 (hence duplicate removed)
 """
-for element in nums:
-    if element not in no_duplicate:  # if the current element is not in the list,
-        no_duplicate.append(element) # add the element to the list
-print(no_duplicate)
+nums = [1, 3, 67, 89, 1, 3, 45, 66]
+
+def remove_dup(nums):
+    no_duplicate = []               # every number stored here will occur once
+
+    for i in nums:
+        if i not in no_duplicate:   # if the current element is not in the list, USE not in INSTEAD OF !=
+            no_duplicate.append(i)  # add the element to the list
+    return no_duplicate             # will output thr new list with no duplicates
 
 
-
-
+print(remove_dup(nums))
