57cea7c5cb666c22f5d98f332829c950a9ec010c
Oboni ANower <oboni_anower@yahoo.com>
2022-08-14 17:03:18 +0100

    added SET method

--- LIST/Exercises/removes dupliocate.py
+++ LIST/Exercises/removes dupliocate.py
@@ -8,6 +8,7 @@ will print the list with every number occurring only once
 (hence duplicate removed)
 """
 nums = [1, 3, 67, 89, 1, 3, 45, 66]
+print('Given list: ', nums)
 
 def remove_dup(nums):
     no_duplicate = []               # every number stored here will occur once
@@ -18,4 +19,15 @@ def remove_dup(nums):
     return no_duplicate             # will output thr new list with no duplicates
 
 
-print(remove_dup(nums))
+print('After removing the duplicate numbers: ',remove_dup(nums))
+
+
+
+# using SET method
+nums1 = [1, 3, 67, 89, 1, 3, 45, 66]
+print('Given list: ', nums1)
+
+nums1_set = set(nums1)                # making a SET
+print('Unique SET of numbers: ', nums1_set)                      # prints the SET (no duplicates)
+nums1_set_to_list = list(nums1_set)   # converting from SET to list
+print('Unique LIST of numbers: ',nums1_set_to_list)              # prints the list with no duplicates/unique numbers
