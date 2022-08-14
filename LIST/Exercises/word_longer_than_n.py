1b3862de45f795724406686a08dd5aaf22b92d3d
Oboni ANower <oboni_anower@yahoo.com>
2022-08-14 18:47:04 +0100

    longer than n

---
+++ LIST/Exercises/word_longer_than_n.py
@@ -0,0 +1,30 @@
+"""
+Write a program to find the list of words that are longer than n from a given list of words.
+"""
+
+name_list = ['kiwi', 'banana', 'apple', 'orange', 'mango', 'pineapple']
+
+def longer_word(name_list, n):
+    new_list = []                    # new empty list created
+
+    for i in name_list:              # for the elements in name_list
+        if len(i) > n:               # if the length of element is greater than n
+            new_list.append(i)       # add that element to the new list
+    return new_list                  # return the new list
+
+
+print(longer_word(name_list, 5))     # n=5 assigned
+
+
+
+# -------------------------SPLIT method-----------------------------
+def long_words(n, str):
+    word_len = []
+    txt = str.split(" ")
+
+    for x in txt:
+        if len(x) > n:
+            word_len.append(x)
+    return word_len
+
+print(long_words(3, "The quick brown fox jumps over the lazy dog"))
