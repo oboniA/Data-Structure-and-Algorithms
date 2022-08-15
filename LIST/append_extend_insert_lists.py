638320648598b5a711687329457bdf8e5afb3ffc
Oboni ANower <oboni_anower@yahoo.com>
2022-08-15 13:24:18 +0100

    new append method added

--- LIST/append_extend_insert_lists.py
+++ LIST/append_extend_insert_lists.py
@@ -1,17 +1,26 @@
+# ----------------APPENDS------------------------------
 x = [2, 4, 6, 7, 8]   #  list created
 x.append(11)          #  11 is added to the end of the list
 print(x)
 
 xx = [2, 4, 6, 7, 8]
-xx.append((12, 13))   # thsi tuple is added to the list
+xx.append((12, 13))   # this tuple is added to the list
 print(xx)
 
+list1 = [1, 2, 3, 0]
+list2 = ['R', 'G', 'B']
+merge_list = list1 + list2
+print(merge_list)
+
+
+# --------------------EXTENDS----------------------------
 x1 = [2, 4, 6, 7, 8]
 y1 = [90, 67]
 x1.extend(y1)
 print(x1)
 
 
+# --------------------INSERTS-------------------------------
 z = [2, 4, 6, 7, 8]
 z.insert(3, -4)       # adds -4 to index3
 print(z)
