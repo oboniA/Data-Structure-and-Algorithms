748bde91a254c03b605f70620ac13571d16343dc
Oboni ANower <oboni_anower@yahoo.com>
2022-08-15 13:13:43 +0100

    find the index value of elements in the list

---
+++ LIST/find_index.py
@@ -0,0 +1,7 @@
+# INDEX of a single element
+list1 = [10, 30, 4, -6, 7, 90]
+print(list1.index(30))
+
+# INDEX of all the elements
+for list1_index, list1_nums in enumerate(list1):
+    print(list1_index, list1_nums)
