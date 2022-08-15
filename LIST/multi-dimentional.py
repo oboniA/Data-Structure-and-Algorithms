3760ab45ee7eead3cc0842b0440f36aa01c24c0c
Oboni ANower <oboni_anower@yahoo.com>
2022-08-15 13:17:26 +0100

    FLATTEN a SHALLOW LIST

--- LIST/multi-dimentional.py
+++ LIST/multi-dimentional.py
@@ -2,3 +2,11 @@ coords = [[1,2,3,4], [5,6,7,8]]  # 0th list: 0 1 2 3 index, 1st list: 0 1 2 3 in
 print(coords)
 print(coords[1][2])   # give the value from 1st Row and 2nd column
                       # OR, gives, the index2 from 1st list
+
+
+# FLATTEN a SHALLOW LIST
+import itertools
+list1 = [[2,4,3],[1,5,6], [9], [7,9,0]]
+new_list = list(itertools.chain(*list1))
+print(new_list)
+print(list1[3][1])
