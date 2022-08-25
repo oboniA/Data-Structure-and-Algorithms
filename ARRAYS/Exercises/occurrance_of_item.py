d7243f18c9c8360305f4180317a2255f11e4594c
Oboni ANower <oboni_anower@yahoo.com>
2022-08-25 16:29:57 +0100

    count() Built-in function

---
+++ ARRAYS/Exercises/occurrance_of_item.py
@@ -0,0 +1,8 @@
+"""
+Write a Python program to get the number of occurrences of a specified element in an array.
+"""
+
+from array import *
+array_num = array('i', [1, 3, 5, 3, 7, 9, 3])
+print("Original array: ", array_num)
+print("Number of occurrences of the number 3 in the array: ", array_num.count(3))
