c743d76b9addc73541b9fdd5f6bf626401bf6547
Oboni ANower <oboni_anower@yahoo.com>
2022-08-25 16:33:57 +0100

    counts occurrence using algorithm

--- ARRAYS/Exercises/occurrance_of_item.py
+++ ARRAYS/Exercises/occurrance_of_item.py
@@ -3,6 +3,20 @@ Write a Python program to get the number of occurrences of a specified element i
 """
 
 from array import *
-array_num = array('i', [1, 3, 5, 3, 7, 9, 3])
-print("Original array: ", array_num)
-print("Number of occurrences of the number 3 in the array: ", array_num.count(3))
+
+array_num = array('i', [1, 3, 3, 5, 3, 7, 9, 3])
+
+arr1 = array_num
+print(arr1)
+
+count = 0
+count_item = 3
+
+for i in arr1:                                          # for elements in the list
+    if count_item == i:                                 # if the count_item is same as the element:i
+        count += 1                                      # add 1 to the number of counts (increments)
+
+if count == 0:                                          # if counts 0 times,
+   print(f"{count_item} do not occur in this list")
+else:                                                   # counts once or more
+   print(f"{count_item} repeats {count} times")
