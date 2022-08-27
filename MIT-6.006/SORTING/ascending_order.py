375169718a752405fb4e447c70cfb89a10e6dd86
Oboni ANower <oboni_anower@yahoo.com>
2022-08-27 16:29:37 +0100

    algorithm method using swaps

--- MIT-6.006/SORTING/ascending_order.py
+++ MIT-6.006/SORTING/ascending_order.py
@@ -1,5 +1,17 @@
-list1 = [5, 7, 90, 45, 32, 67, 5, 1, 7, 45]
+"""
+Sort a given list of items in Ascending order
+  -- replace > with < for descending order
+"""
 
-list11 = list1
-list11.sort()
-print(list11)
+list1 = [5, 7, 9, 4, 3, 6]
+print("Unsorted original list:",list1)
+
+i = 0                                      # initialized to zero; the first i is 0th index; that means first i+1 is 1st index
+for i in range(len(list1)):                # for all the items in the list
+    for j in range(i+1, len(list1)):       # for item between the next and last in the list
+        if list1[i] > list1[j]:            # if the present item is greater than the next one
+            t = list1[i]                   # t takes the value of the i
+            list1[i] = list1[j]            # i takes the value of the j
+            list1[j] = t                   # j takes the value of t
+
+print("Sorted in ascending order:",list1)
