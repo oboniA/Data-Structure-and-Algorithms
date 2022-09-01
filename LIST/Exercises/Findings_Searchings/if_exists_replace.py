f0db97742bdeeb471a2b98948a8b744564e9989b
Oboni ANower <oboni_anower@yahoo.com>
2022-09-01 19:28:15 +0100

    for loop

---
+++ LIST/Exercises/Findings_Searchings/if_exists_replace.py
@@ -0,0 +1,15 @@
+"""
+ou have given a Python list.
+Write a program to find value 20 in the list,
+and if it is present, replace it with 200.
+only replace the first occurrence
+"""
+
+list1 = [5, 10, 15, 20, 25, 50, 20]
+count = 0
+
+for i in range(len(list1)):
+    if list1[i] == 20:
+        list1[i] = 200
+        print(list1)
+        break
