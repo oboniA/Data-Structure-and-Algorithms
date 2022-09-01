fdef59cb116e81ff73c96a11bfd901893825b5f6
Oboni ANower <oboni_anower@yahoo.com>
2022-09-01 19:47:37 +0100

    filtering

--- LIST/Exercises/Removing techniques/remove_empty_str.py
+++ LIST/Exercises/Removing techniques/remove_empty_str.py
@@ -1,7 +1,6 @@
 list1 = ["Mike", "", "Emma", "Kelly", "", "Brad"]
 new_list = []
 
-for i in list1:
-    if len(i) >= 1:
-        new_list.append(i)
-print(new_list)
+# remove None from list1 and convert result into list
+res = list(filter(None, list1))
+print(res)
