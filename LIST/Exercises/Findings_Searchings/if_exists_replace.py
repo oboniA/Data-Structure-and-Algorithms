9e254b7004ac6bf07fa59d0c966a1adb9d042931
Oboni ANower <oboni_anower@yahoo.com>
2022-09-01 19:30:12 +0100

    index method

--- LIST/Exercises/Findings_Searchings/if_exists_replace.py
+++ LIST/Exercises/Findings_Searchings/if_exists_replace.py
@@ -7,8 +7,10 @@ only replace the first occurrence
 
 list1 = [5, 10, 15, 20, 25, 50, 20]
 
-for i in range(len(list1)):
-    if list1[i] == 20:
-        list1[i] = 200
-        print(list1)
-        break
+# get the first occurrence index
+index = list1.index(20)
+print(index)
+
+# update item present at location
+list1[index] = 200
+print(list1)
