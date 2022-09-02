de0f61112ee815063144afcea92dd4bfa0d64705
Oboni ANower <oboni_anower@yahoo.com>
2022-09-02 15:33:57 +0100

    list comprehension examples

--- LIST/list_comprehension.py
+++ LIST/list_comprehension.py
@@ -2,8 +2,53 @@
 are used for creating new lists from other iterables like tuples, strings, arrays, lists, etc
 list comprehension consists of brackets containing the expression, which is executed for each count_item along with the for loop to iterate over each count_item.
 
-newList = [ expression(count_item) for count_item in iterable( ]
+newList = [ expression(count_item) for count_item in iterable(if/for) ]
 """
 
-list1 = [i*i for i in range(5)]
-print(list1)
+
+
+'''
+(values) = [ (expression) for (value) in (collection) ]
+
+alues = []
+for (value) in (collection):
+     values.append(expression)    
+'''
+
+# For loop
+sqr_list1 = []
+for i in range(5):
+    sqr_list1.append( i * i )
+print(sqr_list1)
+
+# List Comprehension
+sqr_list2 = [i * i for i in range(5)]    # gives square numbers from 0-4
+print(sqr_list2)
+
+
+
+# ------------------------------------------------------------------------------
+'''
+(values) = [ (expression) for (value) in (collection) if (condition) ]
+
+values = []
+for (value) in (collection):
+      if (condition):
+          values.append(expression)     
+'''
+
+# For loop
+even_sqrs = []
+for x in range(10):
+    if x % 2 == 0:
+        even_sqrs.append( x * x )
+print(even_sqrs)
+
+# List Comprehension
+evn_sqr = [(x * x) for x in range(10) if (x % 2 == 0)]
+print(evn_sqr)
+
+evnsqr = [(x * x)
+          for x in range(10)
+          if (x % 2 == 0)]        # Good practice
+print(evnsqr)
