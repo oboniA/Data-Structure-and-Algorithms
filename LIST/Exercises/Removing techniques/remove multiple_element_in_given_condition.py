1b877d742538007852e77b2e5247567706a8b1db
Oboni ANower <oboni_anower@yahoo.com>
2022-09-03 18:46:39 +0100

    removing with conditions

---
+++ LIST/Exercises/Removing techniques/remove multiple_element_in_given_condition.py
@@ -0,0 +1,14 @@
+list1 = [11, 3,  5, 17, 18, 23, 50]
+
+for i in list1:
+    if (i == 5) or (i == 11) :
+        list1.remove(i)
+print(list1)
+
+
+
+
+
+list2 = [11, 3,  5, 17, 18, 23, 50]
+del list2[2:4]
+print(list2)
