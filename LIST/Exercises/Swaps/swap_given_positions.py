3c944cf9b006821556cc5eeb1ef5b9cc7c68fcdc
Oboni ANower <oboni_anower@yahoo.com>
2022-09-03 15:26:00 +0100

    pop; insert

--- LIST/Exercises/Swaps/swap_given_positions.py
+++ LIST/Exercises/Swaps/swap_given_positions.py
@@ -5,10 +5,18 @@ write a program to swap the two elements in the list.
 
 def swap_position(a, pos1, pos2):
 
-    a[pos1], a[pos2] = a[pos2], a[pos1]
+    # popping both the elements from list
+    first_element = a.pop(pos1)
+    second_element = a.pop(pos2-1)
+
+    # inserting in each others positions
+    a.insert(pos1, second_element)
+    a.insert(pos2, first_element)
+
     return a
 
 
 list1 = [23, 65, 19, 90]
 pos1, pos2  = 1, 3
+
 print(swap_position(list1, pos1-1, pos2-1))
