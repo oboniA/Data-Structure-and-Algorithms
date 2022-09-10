826c81ead37312852ebc24dcd05854495de9341d
Oboni ANower <oboni_anower@yahoo.com>
2022-09-10 16:52:31 +0100

    join() + replace() + split()

--- LIST/Exercises/Swaps/swaps_everywhere.py
+++ LIST/Exercises/Swaps/swaps_everywhere.py
@@ -1,5 +1,17 @@
+"""
+wherever there is a B(case-sensitive), swap with e(case-sensitive)
+and vice versa
+for all the items
+"""
+
 list1 = ['Best', 'Ball', 'Egg', 'bEn', 'bEntly', 'eBay']
 print('The original list: ', list1)
 
-swap_list = [sub.replace('B', '-').replace('e', 'B').replace('-', 'e') for sub in list1]
+
+# joins all the items with a $
+# swaps
+# splits at every $ to get back the list
+joint_list = " $ ".join(list1)
+print(joint_list)
+swap_list = joint_list.replace("B", "_").replace("e", "B").replace("_", "e").split(' $ ')
 print('After swapping: ', swap_list)
