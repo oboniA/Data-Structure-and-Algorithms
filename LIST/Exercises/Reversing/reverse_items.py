26bf6a592ab46bb703a293e556587c0e7870147a
Oboni ANower <oboni_anower@yahoo.com>
2022-09-10 17:29:17 +0100

    mapping

--- LIST/Exercises/Reversing/reverse_items.py
+++ LIST/Exercises/Reversing/reverse_items.py
@@ -1,5 +1,6 @@
 list1 = ['My','Name','is', 'something']
 print('original list: ', list1)
 
-item_reversed = [i[::-1] for i in list1]
-print('after reversing: ', item_reversed)
+item_reversed = lambda i : i[::-1]
+reversed = list(map(item_reversed, list1))
+print('after reversing: ', reversed)
