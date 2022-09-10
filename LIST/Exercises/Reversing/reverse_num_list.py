161b92e8720b7c59ce771d3e4d97026104c41e29
Oboni ANower <oboni_anower@yahoo.com>
2022-09-10 17:05:33 +0100

    pointer

--- LIST/Exercises/Reversing/reverse_num_list.py
+++ LIST/Exercises/Reversing/reverse_num_list.py
@@ -1,3 +1,19 @@
-list1 = [105, 72, 200, 139, 300, 620, 400, 3]
-reverse_list = list1[::-1]
-print(reverse_list)
+list1 = [105, 'sD', 200, 139, 'Queen', 620, 400, 3]
+print('Original list: ', list1)
+
+def reverse_list(l):
+    i = 0                # first item
+    j = len(l) - 1       # last item
+
+    while i < j:
+        t = l[i]
+        l[i] = l[j]
+        l[j] = t
+
+        i += 1
+        j -= 1
+
+reverse_list(list1)
+print('After reversing', list1)
+
+
