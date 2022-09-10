0d02eb06fcced30bfb8091727b6d6d63b98b5e27
Oboni ANower <oboni_anower@yahoo.com>
2022-09-10 16:16:45 +0100

    zip

--- LIST/Exercises/Concatenations/simultaneously_reversed.py
+++ LIST/Exercises/Concatenations/simultaneously_reversed.py
@@ -1,7 +1,7 @@
 """
 Given a two Python list.
 Write a program to iterate both lists simultaneously
-and display items from list1 in original order
+and display items from list2 in original order
 and items from list2 in reverse order.
 """
 
