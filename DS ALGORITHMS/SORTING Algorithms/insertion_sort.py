40fc85a05ecdd8091be2d243b669726ca1cb9cdc
Oboni ANower <oboni_anower@yahoo.com>
2022-09-10 20:07:19 +0100

    directory changed

--- DS ALGORITHMS/SORTING Algorithms/insertion_sort.py
+++ DS ALGORITHMS/SORTING Algorithms/insertion_sort.py
@@ -1,15 +1,20 @@
 """
- -Another way of sorting an unsorted list
+ -Another way of sorting an unsorted list, as a new sorted list
   --in ascending/descending order
+
  - Every INDEX that is being sorted is a STEP in iteration
+
  - The first element in the array is assumed to be sorted.
    -- Take the second element and store it separately in key.
+
  - Compare key with the first element.
    -- If the first element is greater than key, then key is placed in front of the first element.
+
  - the first two elements are sorted.
    -- Take the third element and compare it with the elements on the left of it.
    --Place it just behind the element smaller than it.
      -- If there is no element smaller than it, then place it at the beginning of the array.
+
  - Similarly, place every unsorted element at its correct position.
 
 Source: https://www.programiz.com/dsa/insertion-sort
@@ -19,16 +24,20 @@ Source: https://www.programiz.com/dsa/insertion-sort
 
 def insert_sort(array1):
 
-    for i_step in range(1, len(array1)):                     # for (initial) unsorted list
-        key = array1[i_step]                                 # the current item of the current step
-        j = i_step - 1                                       # the immediate previous item of i_step
+    for i_step in range(1, len(array1)):       # for (initial) unsorted list
+        key = array1[i_step]                   # the current item of the current step
+        j = i_step - 1                         # the immediate previous item
 
         # ascending order
-        while (j >= 0) and (key < array1[j]):                # when present item is smaller than the item to the immediate LHS (and is greater than 0)
-            array1[j + 1] = array1[j]                        # swap the value, RHS with LHS
-            j -= 1                                           # goes down the list incrementally
+        while (j >= 0):
+            if (key < array1[j]):              # when present item is smaller than the item to the immediate LHS (as long as the index of j is 0 or more)
+               array1[j+1] = array1[j]         # shift the value in arr[j] to the right
+               array1[j] = key                 # place the key at the recently empty spot
+               j -= 1                          # goes down the list incrementally
 
-        array1[j + 1] = key                                  # Place key at after the element just smaller than it
+            else:
+                break
+
 
 
 items = [7, 5, 1, 3, 9, 10, 2]
