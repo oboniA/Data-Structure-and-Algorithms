a727639b8285f20dc94bd43d4fb2d4a9c8e48695
Oboni ANower <oboni_anower@yahoo.com>
2022-09-10 17:07:39 +0100

    append

--- LIST/Exercises/Reversing/reverse_num_list.py
+++ LIST/Exercises/Reversing/reverse_num_list.py
@@ -1,19 +1,11 @@
 list1 = [105, 'sD', 200, 139, 'Queen', 620, 400, 3]
 print('Original list: ', list1)
 
-def reverse_list(l):
-    i = 0                # first item
-    j = len(l) - 1       # last item
+reversed_list = []
 
-    while i < j:
-        t = l[i]
-        l[i] = l[j]
-        l[j] = t
+for i in range(1, len(list1)+1):
+    reversed_list.append(list1[-i])
 
-        i += 1
-        j -= 1
-
-reverse_list(list1)
-print('After reversing', list1)
+print(reversed_list)
 
 
