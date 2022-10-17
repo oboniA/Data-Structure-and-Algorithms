8ad6ee7fa63347a9282124cb57f7003fbcd65b81
Oboni ANower <oboni_anower@yahoo.com>
2022-10-17 17:41:05 +0100

    heapify with Insertion method

--- DS ALGORITHMS/HEAPS/heapify.py
+++ DS ALGORITHMS/HEAPS/heapify.py
@@ -1,42 +1,59 @@
 """
     Max Heapify
     Source: https://www.youtube.com/watch?v=CUuuBsNqMpw
+            https://www.programiz.com/dsa/heap-data-structure
 """
-# define max_heapify (4)
-def max_heapify(arr, n, i):            # n = size of array, i = position
-    largest = i                        # initiated largest value (root)
-    left = 2*i + 1                     # left of i
-    right =  2*i + 2                   # right of i
-
-    # largest so far is compared with the left (5)
-    if left < n and arr[left] > arr[i]:
-        largest = left                 # left is now the largest
-
-    # largest so far is compared with the right (6)
-    if right < n and arr[right] > arr[i]:
-        largest = right                # right is now the largest
-
-    # swap the new largest with the previous largest: swap parent with child (7)
-    if largest != i:                   # swapping the new with the old
-        arr[i], arr[largest] = arr[largest], arr[i]
-
-        # recursive call (8)
-        max_heapify(arr, n, largest)
-
-
 # driver code: executes all the functions (1)
-arr = [22, 37, 5, 92, 1, 12]           # the array
+arr = []                               # the array
 n = len(arr)                           # size of the array
 
 
-# build max heap (2)
+# define max_heapify (3)
+def max_heapify(arr, n, i):            # n = size of array, i = position
+    largest = i                        # initiated the largest value (root)
+    left = 2 * i + 1                   # left of i
+    right = 2 * i + 2                  # right of i
+
+    # largest so far is compared with the left
+    if left < n and arr[left] > arr[i]:
+        largest = left                 # left is now the largest
+
+    # largest so far is compared with the right
+    if right < n and arr[right] > arr[i]:
+        largest = right                # right is now the largest
+
+    # swap the new largest with the previous largest: swap parent with child
+    if largest != i:                   # swapping the new with the old
+        arr[i], arr[largest] = arr[largest], arr[i]
+        # recursive call (from 2)
+        max_heapify(arr, n, largest)
+
+
+# insertion (4)
+def insert(arr1, num):                 # array and size created again
+    n_size = len(arr1)
+    if n_size == 0:                    # if array empty
+        arr1.append(num)               # add insert the number to the array list
+    else:                              # if not empty
+        arr1.append(num)               # add the num to array list
+        # recursive call (from 2)      # and do the heapify so the number is placed to the correct position
+        for i in range((n_size // 2 - 1), -1, -1):
+            max_heapify(arr1, n_size, i)
+
+
+# build max heap (2) : this will be called at every Method
 for i in range(n//2-1, -1, -1):        # range starts from a, ends at b, c = value decrements by 1
-    max_heapify(arr, n, i)             # call the heapify func (won't exist, so need to define(4) it afterwards)
+    max_heapify(arr, n, i)             # heapify func that will be called (won't exist, so need to define(4) it afterwards)
 
 
-# display (3)
-print("Max heap is: ")
-for i in range(n):                     # entire array
-    print(arr[i])                      #
+# the arrays are ready to be inserted (5)
+insert(arr, 22)
+insert(arr, 37)
+insert(arr, 52)
+insert(arr, 92)
+insert(arr, 1)
+insert(arr, 12)
 
 
+# displays (6)
+print("Max heap is: " + str(arr))
