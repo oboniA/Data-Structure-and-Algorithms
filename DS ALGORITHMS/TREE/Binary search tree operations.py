69e4e0a9d45a9246eba384f5ec639507ddb4ebc5
Oboni ANower <oboni_anower@yahoo.com>
2022-10-18 16:18:41 +0100

    Created the BST (ascending)

---
+++ DS ALGORITHMS/TREE/Binary search tree operations.py
@@ -0,0 +1,80 @@
+"""
+    Binary search tree operations
+
+    - create insert
+    - tree traverse
+    - build the tree
+    - print the tree (sorted) (ascending: root is the smallest)
+    Source: https://www.programiz.com/dsa/binary-search-tree
+            https://www.youtube.com/watch?v=lFq5mYUWEBk&t=665s
+"""
+
+# creating class (2)
+class BSTNode:
+    # just for a node, without any left or right
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # method for inserting values to the tree (3)
+    def insert(node, value):
+        if value == node.value:                   # if the value already exists
+            return                                # returns, prevents duplicates
+
+        if value < node.value:                    # if the value is less than it's parent node
+            # add data to left subtree
+            if node.left:                         # if left node contains value (not leaf-node)
+                node.left.insert(value)           # recursion of (3): starts creating sub-tree
+            else:                                 # if left node is empty (is a leaf-node)
+                node.left = BSTNode(value)        # recursion of (2)
+        else:
+            # add to right subtree
+            if node.right:                        # if right node contains value (not leaf-node)
+                node.right.insert(value)          # recursion of (3)
+            else:                                 # if right node is empty (is a leaf-node)
+                node.right = BSTNode(value)       # recursion of (2)
+
+
+
+    # method for traversal (4)
+    # will return a list of tree elements in specific order
+    def inOrder(node):
+        tree_elements = []                        # elements initiated in empty list
+
+        # visit left tree
+        if node.left:
+            tree_elements += node.left.inOrder()  # recursion of (4) added to the element list
+
+        # visit base node
+        tree_elements.append(node.value)
+
+        # visit right tree
+        if node.right:
+            tree_elements += node.right.inOrder() # recursion of (4) added to the element list
+
+        return tree_elements
+
+
+# building the tree (5)
+def build_tree(tree_elements):
+    root = BSTNode(tree_elements[0])             # assigned first element as the root node
+
+    for i in range(1, len(tree_elements)):       #
+        root.insert(tree_elements[i])
+
+    return root
+
+
+# The numbers in the tree (1)
+if __name__ == '__main__':
+    numbers = [65, 7, 32, 4, 12, 9, 7, 44]
+    tree_numbers = build_tree(numbers)           # calling (5)
+
+    print(tree_numbers.inOrder())                # prints in-order traversal in ascending order; sorts; prints set
+
+
+
+
+
+
