ebd81dfd2b979bf110e97eb694bb7b8cdddb6633
Oboni ANower <oboni_anower@yahoo.com>
2022-11-09 19:17:27 +0000

    traversal method

---
+++ DS ALGORITHMS/LINKED-LIST/Singly.py
@@ -0,0 +1,33 @@
+"""
+Singly Linked List
+ - Traversal of a linked list
+ - no data in the list so returns empty list
+"""
+
+
+# Create Node class (2)
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.ref = None
+
+
+# creating a linked list (3)
+class Linkedlist:
+    def __init__(self):
+        self.head = None
+
+    # writing the traversal method for a linked list (3.1)
+    def print_linked_list(self):
+        if self.head is None:
+            print("Linked list is empty")
+        else:
+            n = self.head
+            while n is not None:
+                print(n.data)
+                n = n.ref  # next head is the next ref_node
+
+
+# Driver code (1)
+list1 = Linkedlist()
+list1.print_linked_list()
